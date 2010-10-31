e9a641fcbbaf1408f875b55ec7e4d0b4b1cfa179
Noé Rubinstein <noe.rubinstein@gmail.com>
2010-10-31 02:16:05 +0200

    use shellwords (untested lol)
    also delete a file lol

--- mplayer-toyunda.rb
+++ mplayer-toyunda.rb
@@ -7,18 +7,14 @@
 # To Public License, Version 2, as published by Sam Hocevar. See
 # http://sam.zoy.org/wtfpl/COPYING for more details.
 
-class String
-  def escape_shell # there must be a less stupid way to do this
-    "'" + gsub("'", "'\"'\"'") + "'"
-  end
-end
+require 'shellwords'
 
 $stderr.puts "THIS FILE IS OUT OF DATE, I doubt it does anything useful but you can always try."
 
 ARGV.each{|arg|
   if File.exists?(arg) && File.exists?(txt_filename = arg.sub(/\.(avi|flv|mkv|mp4)$/, ".txt"))
     puts "TOYUNDA->ASS for file #{arg}"
-    io = IO.popen %(mplayer -slave -quiet -vo null -ao null #{arg.escape_shell}), "r+"
+    io = IO.popen %(mplayer -slave -quiet -vo null -ao null #{arg.shellescape}), "r+"
     io.puts "get_property fps"
     io.puts "get_video_resolution"
     io.puts "quit"
@@ -31,7 +27,7 @@ ARGV.each{|arg|
     end
 
     f = File.open(arg.sub(/\.(avi|mkv|flv|mp4)$/, ".ass"), "w")
-    io = IO.popen("ruby #{File.dirname $0}/toyunda2ass.rb #{txt_filename.escape_shell} #{fps} #{w} #{h}")
+    io = IO.popen("ruby #{File.dirname $0}/toyunda2ass.rb #{txt_filename.shellescape} #{fps} #{w} #{h}")
     f.puts io.read
     f.close
   end
